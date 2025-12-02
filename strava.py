eeac03f4029e5447be3bbba444dd0b3b91bdf815
Benjamin Stager <bstager@tulane.edu>
2025-12-02 08:12:32 -0500

    encapsulation

--- strava.py
+++ strava.py
@@ -36,10 +36,17 @@ class StravaAPI:
         access_token = tokens["access_token"]
         self.headers = {"Authorization": f"Bearer {access_token}"}
 
-    def make_request(self, page, per_page):
+    def make_request_nondetailed(self, page, per_page):
         r = requests.get(self.activies_endpoint, headers=self.headers, params={"page":page, "per_page": per_page})
         activities = r.json()
         return activities
+    
+    def make_request_detailed(self, activity_id):
+        activities_with_id = f"{self.activies_endpoint}/{activity_id}"
+        r = requests.get(activities_with_id, headers=self.headers)
+        specific_activity = r.json()
+        
+        setattr(self, f'activity_{activity_id}', specific_activity) # this may need tweaking based on setattr rules
 
     def pull_activity_data(self, num_activities='all'):
         activities_list = []
@@ -47,7 +54,7 @@ class StravaAPI:
         
         if num_activities == 'all':
             for _ in range(12):
-                response = self.make_request(page=page, per_page=100)
+                response = self.make_request_nondetailed(page=page, per_page=100)
                 activities_list.extend(response)
                 page+=1
         else:
@@ -55,16 +62,20 @@ class StravaAPI:
                 pages = num_activities // 100
                 remainder = num_activities % 100
                 for _ in range(pages-1):
-                    response = self.make_request(page=page, per_page=100)
+                    response = self.make_request_nondetailed(page=page, per_page=100)
                     activities_list.extend(response)
                     page+=1
                 
-                response = self.make_request(page=pages, per_page=remainder)
+                response = self.make_request_nondetailed(page=pages, per_page=remainder)
                 activities_list.extend(response)
             else:
                 page = 1
-                response = self.make_request(page=1, per_page=num_activities)
+                response = self.make_request_nondetailed(page=1, per_page=num_activities)
                 activities_list.extend(response)
-                page+=1
 
-        self.activies_list = activities_list
+        self.activies_list = activities_list # maybe change to return list itself
+    
+
+class StravaML(StravaAPI):
+    def __init__(self, client_id, client_secret, code=None):
+        super().__init__(client_id, client_secret, code)
